2a1b0994bfcd774785de743946435f40df184a86
DOINORDER <117586282+DOINORDER@users.noreply.github.com>
2024-04-08 00:12:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ Shery.textAnimate("#headings h1", {
   //Parameters are optional.
   style: 2,
   y: 10,
-  delay: 0.3,
   duration: 2,
   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
   multiplier: 0.1,
